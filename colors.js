e44d58df35fa9acce4ebec444a8826653c9af2f9
Félix Acero <acerofel@uniandes.edu.co>
2022-11-06 18:11:38 -0500

    Introduccing error 1

--- colors.js
+++ colors.js
@@ -12,24 +12,24 @@
  * @return  Array           The HSL representation
  */
 function rgbToHsl(r, g, b){
-    r /= 255;
+    r /= 255
     g /= 255;
     b /= 255;
-    
-    var max = Math.max(r, g, b), min = Math.min(r, g, b);
-    var h, s, l = (max + min) / 2;
+
+    let max = Math.max(r, g, b), min = Math.min(r, g, b);
+    let h, s, l = (max + min) / 2;
 
     if(max == min){
         h = s = 0; // achromatic
     }else{
-        var d = max - min;
+        let d = max - min;
         s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
         switch(max){
             case r: h = (g - b) / d + (g < b ? 6 : 0); break;
             case g: h = (b - r) / d + 2; break;
             case b: h = (r - g) / d + 4; break;
         }
-        h /= 6;
+        h /= 6
     }
 
     return [h, s, l];
@@ -47,7 +47,7 @@ function rgbToHsl(r, g, b){
  * @return  Array           The RGB representation
  */
 function hslToRgb(h, s, l){
-    var r, g, b;
+    let r, g, b;
 
     if(s == 0){
         r = g = b = l; // achromatic
