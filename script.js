b30e4edd72eb160143254d1f7ea7f005e931e0fe
Félix Acero <acerofel@uniandes.edu.co>
2022-11-16 21:21:11 -0500

    Ajuste 5

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function randomPalette(){
     const dist = 360/numColors;
     const saturation = 1, brightness = 1;
     //Get the first color's hue value
-    var newColor = Math.ceil(Math.random()*359);
-    var colors = []
+    let newColor = Math.ceil(Math.random()*359);
+    let colors = []
     while(colors.length<numColors){
         colors.push([newColor, saturation, brightness])
         newColor = (newColor + dist) % 360
@@ -14,45 +14,37 @@ function randomPalette(){
 }
 
 function generateRules(){
-    var i = 1
-    var colors = randomPalette()
-    var rgbStrings = []
+    let i = 1
+    let colors = randomPalette()
+    let rgbStrings = []
     while(i<=numColors){
         let currentHSV = colors[i-1]
-        let rgb = hsv_ToRgb(currentHSV[0]/360,currentHSV[1],currentHSV[2])
+        let rgb = hsvToRgb(currentHSV[0]/360,currentHSV[1],currentHSV[2])
         let rgbString = `rgb(${rgb[0]},${rgb[1]},${rgb[2]})`
         rgbStrings.push(rgbString)
         document.getElementById(`color${i}`).style.backgroundColor = rgbString
         i++;
-    
+    }
     document.getElementById('css-rules').innerHTML=`
 .website-background{ color: ${rgbStrings[0]};}
-
 .element-text{ color: ${rgbStrings[1]};}
-
 .element-border{ border-color: ${rgbStrings[2]};}
-
 .element-background{ background-color: ${rgbStrings[3]};}
-
 .header{ color: ${rgbStrings[4]};}
         `
 }
 
 function clean(){
-    var i = 1;
+    let i = 1;
     while(i<=numColors){
         document.getElementById(`color${i}`).style.backgroundColor = '#FFFFFF'
         i++;
     }
     document.getElementById('css-rules').innerHTML=`
 .website-background{ color: #FFFFFF;}
-
 .element-text{ color: #FFFFFF;}
-
 .element-border{ border-color: #FFFFFF;}
-
 .element-background{ background-color: #FFFFFF;}
-
 .header{ color: #FFFFFF;}
         `
 }
